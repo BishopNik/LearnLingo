1d67720b2419a8f0fe0eaac1a82cf2637005358b
Andrew <128685383+BishopNik@users.noreply.github.com>
2024-04-01 17:21:22 +0200

    fix teachers

--- src/pages/Teachers.jsx
+++ src/pages/Teachers.jsx
@@ -3,10 +3,11 @@
 import Teacher from 'components/Teacher';
 import React from 'react';
 import data from 'components/data/teachers.json';
+import styles from 'components/styles/teachers.module.css';
 
 function Teachers() {
 	return (
-		<div style={{ backgroundColor: '#F8F8F8', marginBottom: '32px' }}>
+		<div className={styles.main}>
 			<Teacher teacher={data[0]} />
 			<Teacher teacher={data[1]} />
 			<Teacher teacher={data[2]} />
